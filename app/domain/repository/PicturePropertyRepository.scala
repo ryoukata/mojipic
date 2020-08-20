a276c354b47024eb203dcc088c4ab1a30e55e5c0
ryoukata <ryoya3091@gmail.com>
2020-08-20 22:20:04 +0900

    feat(#15): add function changing property information when file is converted.

--- app/domain/repository/PicturePropertyRepository.scala
+++ app/domain/repository/PicturePropertyRepository.scala
@@ -11,4 +11,22 @@ trait PicturePropertyRepository {
    * @return Future.sccessfull(PictureId) 新しく割り当てられた画像ID
    */
   def create(value: PictureProperty.Value): Future[PictureId]
+
+  /**
+   * 画像のプロパティを読み込む
+   *
+   * @param pictureId 画像ID
+   * @return Future.successfull(PictureProperty) 読み込みに成功した
+   */
+  def find(pictureId: PictureId): Future[PictureProperty]
+
+  /**
+   * 画像を更新する
+   *
+   * @param pictureId 画像ID
+   * @param value 値
+   * @return Future.sccessfull(()) 更新に成功した
+   */
+  def update(pictureId: PictureId, value: PictureProperty.Value): Future[Unit]
+
 }
--- app/infrastructure/repository/PicturePropertyRepositoryImpl.scala
+++ app/infrastructure/repository/PicturePropertyRepositoryImpl.scala
@@ -1,6 +1,7 @@
 package infrastructure.repository
 
-import domain.entity.{PictureId, PictureProperty}
+import com.google.common.net.MediaType
+import domain.entity.{PictureId, PictureProperty, TwitterId}
 import domain.repository.PicturePropertyRepository
 import scalikejdbc._
 
@@ -41,5 +42,68 @@ class PicturePropertyRepositoryImpl extends PicturePropertyRepository {
       }
     })
   }
+
+  def find(pictureId: PictureId): Future[PictureProperty] =
+    Future.fromTry(Try {
+      using(DB(ConnectionPool.borrow())) { db =>
+        db.readOnly { implicit sesstion =>
+          val sql =
+            sql"""SELECT
+              | picture_id,
+              | status,
+              | twitter_id,
+              | file_name,
+              | content_type,
+              | overlay_text,
+              | overlay_text_size,
+              | original_filepath,
+              | converted_filepath,
+              | created_time
+              | FROM picture_properties WHERE picture_id = ${pictureId.value}
+               """.stripMargin
+          sql.map(resultSetToPictureProperty).single().apply()
+            .getOrElse(throw new RuntimeException(s"Picture is not found. PictureId: ${pictureId.value}"))
+        }
+      }
+    })
+
+  private[this] def resultSetToPictureProperty(rs: WrappedResultSet): PictureProperty = {
+    val value =
+      PictureProperty.Value(
+        PictureProperty.Status.parse(rs.string("status")).get,
+        TwitterId(rs.long("twitter_id")),
+        rs.string("file_name"),
+        MediaType.parse(rs.string("content_type")),
+        rs.string("overlay_text"),
+        rs.int("overlay_text_size"),
+        rs.stringOpt("original_filepath"),
+        rs.stringOpt("converted_filepath"),
+        rs.localDateTime("created_time")
+      )
+    PictureProperty(PictureId(rs.long("picture_id")), value)
+  }
+
+  def update(pictureId: PictureId, value: PictureProperty.Value): Future[Unit] =
+    Future.fromTry(Try {
+      using(DB(ConnectionPool.borrow())) { db =>
+        db.localTx { implicit session =>
+          val sql =
+            sql"""UPDATE picture_properties SET
+                  | status = ${value.status.value},
+                  | twitter_id = ${value.twitterId.value},
+                  | file_name = ${value.fileName},
+                  | content_type = ${value.contentType.toString},
+                  | overlay_text = ${value.overlayText},
+                  | overlay_text_size = ${value.overlayTextSize},
+                  | original_filepath = ${value.originalFilePath.getOrElse("")},
+                  | converted_filepath = ${value.convertedFilePath.getOrElse("")},
+                  | created_time = ${value.createdTime}
+                  | WHERE picture_id = ${pictureId.value}
+               """.stripMargin
+          sql.update().apply()
+          ()
+        }
+      }
+    })
 
 }
